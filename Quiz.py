11ad2da6917037ceb5fdce93f54ad25cae985152
Manish Sarikonda <sarikondamanish45@gmail.com>
2026-02-12 18:46:17 +0000

    Quiz

---
+++ Quiz.py
@@ -0,0 +1,34 @@
+print("JAI SHREE RAM!!\nWelcome to the Quiz")
+name = input("Please enter your name") 
+print(f"Hello {name}!, lets get started")
+print("1)Ramayana was written by?")
+answer = input("your answer:")
+if answer.lower() == "valmiki":
+    print("Correct — well done!")
+else :
+    print("the correct answer is valmiki")
+print("2)Who is the mother of Karna?")
+answer = input("your answer:")
+if answer.lower() == "Kunti devi":
+    print("That’s right!")
+else :
+     print("the correct answer is Kunti devi")
+print("3)What is the real name of Bhishma pitamah?")
+answer = input("your answer:")
+if answer.lower()== "Devavrata":
+    print("Great going!")
+else :
+     print("the correct answer is Devavrata")
+print("4)Who is the prime minister of india?")
+answer = input("your answer:")
+if answer.lower()=="Narendra modi":
+     print("Bullseye!")
+else :
+     print("The answer is Narendra modi")
+print("5)What is the name of your country?")
+answer = input("your answer:")
+if answer.lower()=="bharath":
+     print("Awesome — you got it!")
+else :
+     print("The answer is bharath")
+print("\nQuiz Completed!")
