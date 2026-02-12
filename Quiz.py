fe1e1c46d82cba4a8e64118a66868fd47063db14
Manish Sarikonda <sarikondamanish45@gmail.com>
2026-02-13 00:41:45 +0530

    Fix print statements and update scoring logic

--- Quiz.py
+++ Quiz.py
@@ -1,34 +1,41 @@
 print("JAI SHREE RAM!!\nWelcome to the Quiz")
+score = 0
 name = input("Please enter your name") 
 print(f"Hello {name}!, lets get started")
 print("1)Ramayana was written by?")
 answer = input("your answer:")
 if answer.lower() == "valmiki":
     print("Correct — well done!")
+    score += 1
 else :
     print("the correct answer is valmiki")
 print("2)Who is the mother of Karna?")
 answer = input("your answer:")
 if answer.lower() == "kunti devi":
-    print("That’s right!")
+    print("That's right!")
+    score += 1
 else :
-     print("the correct answer is Kunti devi")
+    print("the correct answer is Kunti devi")
 print("3)What is the real name of Bhishma pitamah?")
 answer = input("your answer:")
 if answer.lower()== "devavrata":
     print("Great going!")
+    score += 1
 else :
-     print("the correct answer is Devavrata")
+    print("the correct answer is Devavrata")
 print("4)Who is the prime minister of india?")
 answer = input("your answer:")
 if answer.lower()=="narendra modi":
-     print("Bullseye!")
+    print("Bullseye!")
+    score += 1
 else :
-     print("The answer is Narendra modi")
+    print("The answer is Narendra modi")
 print("5)What is the name of your country?")
 answer = input("your answer:")
 if answer.lower()=="bharath":
-     print("Awesome — you got it!")
+    print("Awesome — you got it!")
+    score += 1
 else :
-     print("The answer is bharath")
+    print("The answer is bharath")
+print(f"Your final score is {score} out of 5.")
 print("\nQuiz Completed!")
